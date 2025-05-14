596390f63c7c2f4dc0259df4f053a7443102b594
devjerry <sjhuynh500@gmail.com>
2025-05-14 15:24:28 -0500

    Update routing for tracking page and enhance order details display

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -28,7 +28,7 @@ export function OrderDetailsGrid({ order }) {
               </button>
             </div>
             <div className="product-actions">
-              <a href="/tracking/">
+              <a href={`/tracking/${order.id}/${orderProduct.product.id}`}>
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
