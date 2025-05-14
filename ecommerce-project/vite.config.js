71430fa2e0a4a8ec399e6bc7e0e7fb92824a6089
devjerry <sjhuynh500@gmail.com>
2025-05-13 20:52:47 -0500

    add server proxy config for /api and /images

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxi: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+      "/images": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+});
