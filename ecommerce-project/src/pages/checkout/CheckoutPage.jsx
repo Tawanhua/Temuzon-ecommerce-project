7da25c1fa619debc85743930b222c6e2ce77cb33
devjerry <sjhuynh500@gmail.com>
2025-05-15 16:38:03 -0500

    Implement code changes to enhance functionality and improve performance

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -5,23 +5,29 @@ import { OrderSummary } from "./checkout-order-summary/OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
 import "./CheckoutPage.css";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
-    const fetchCheckoutData = async () => {
-      let response = await axios.get(
-        "/api/delivery-options?expand=estimatedDeliveryTime"
-      );
-      setDeliveryOptions(response.data);
-
-      response = await axios.get("/api/payment-summary");
+    const fetchPaymentSummary = async () => {
+      const response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
     };
 
-    fetchCheckoutData();
+    fetchPaymentSummary();
+  }, [cart]);
+
+  useEffect(() => {
+    const fetchDeliveryOptions = async () => {
+      const response = await axios.get(
+        "/api/delivery-options?expand=estimatedDeliveryTime"
+      );
+      setDeliveryOptions(response.data);
+    };
+    fetchDeliveryOptions();
   }, []);
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/cart-favicon.png" />
@@ -32,7 +38,11 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary deliveryOptions={deliveryOptions} cart={cart} />
+          <OrderSummary
+            deliveryOptions={deliveryOptions}
+            cart={cart}
+            loadCart={loadCart}
+          />
 
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
